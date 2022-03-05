b626d7f841dbde1e0bad5ade5705c9a341394047
dmac24 <dinitayala@gmail.com>
2022-03-05 20:47:45 +0000

    *args, **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,19 @@ import uuid
 
 class BaseModel:
     """Define attributes class from future classes will be derived"""
-    def __init__(self):
-        """ BaseModel Class """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
+    def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
+        if kwargs:
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            
     def __str__(self):
         """Representation of the BaseModel class"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
