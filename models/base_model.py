2f9ec664545e48d98610ef6a35c2357c8d70239b
DanielCeron-Holberton <3403@holbertonschool.com>
2022-03-06 21:58:28 -0500

    [FIX] - Code style

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,10 @@ class BaseModel:
             del kwargs["__class__"]
             for key, value in kwargs.items():
                 setattr(self, key, value)
-            self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
-            self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(self.updated_at,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
+            self.created_at = datetime.strptime(self.created_at,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
