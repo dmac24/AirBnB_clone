633179d20c32b7b605f9d70dfbe457ae671e052f
DanielCeron-Holberton <3403@holbertonschool.com>
2022-03-06 21:09:53 -0500

    [ADD] - Classes on console function

--- console.py
+++ console.py
@@ -4,21 +4,22 @@
 import cmd
 from logging import exception
 import os
-import json
 import shlex
 
 from attr import attr
 # import models
 from models.base_model import BaseModel
-# from models.user import User
-# from models.state import State
-# from models.city import City
-# from models.amenity import Amenity
-# from models.place import Place
-# from models.review import Review
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
-dict_class = {"BaseModel": BaseModel()}
+dict_class = {"BaseModel": BaseModel(), "User": User(), "State": State(),
+              "City": City(), "Amenity": Amenity(), "Place": Place(), 
+              "Review": Review()}
 
 
 class HBNBCommand(cmd.Cmd):
