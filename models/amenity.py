b4d608c213d04c0f6d2993552fe14e7fd9ac6dc6
DanielCeron-Holberton <3403@holbertonschool.com>
2022-03-08 18:48:24 -0500

    [FIX] - Class Attrb fixed

--- models/amenity.py
+++ models/amenity.py
@@ -5,8 +5,9 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """ Amenity """
+    name = ""
+
     def __init__(self, *args, **kwargs):
         """ initialize """
-        if len(kwargs) == 0:
-            self.name = ""
+
         super().__init__(*args, **kwargs)
